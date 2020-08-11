6d9f93e2fdffbdd96559e8eccc02bbf9ea9fce02
github Ryoma1115 <t9831703@gmail.com>
2020-08-11 11:20:22 +0900

    Uped web server.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var e = createMux()
 
 func main() {
-	fmt.Println("Hello World!")
+	// `/` というパス（URL）と `articleIndex` という処理を結びつける
+	e.GET("/", articleIndex)
+
+	// Webサーバーをポート番号 8080 で起動する
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMux() *echo.Echo {
+	// アプリケーションインスタンスを生成
+	e := echo.New()
+
+	// アプリケーションに各種ミドルウェアを設定
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	// アプリケーションインスタンスを返却
+	return e
+}
+
+func articleIndex(c echo.Context) error {
+	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
+	return c.String(http.StatusOK, "Hello, World!")
 }
