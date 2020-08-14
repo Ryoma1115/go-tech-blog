f150103631d2b12c9c68775fb18eabe3331530a7
github Ryoma1115 <t9831703@gmail.com>
2020-08-14 21:19:35 +0900

    Add handler/article_handler.go and handler/render.go.

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/flosch/pongo2 v0.0.0-20200805083417-63c99409991d
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.16
 )
--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/flosch/pongo2"
+	"go-tech-blog/handler"
+	"go-tech-blog/repository"
+
 	_ "github.com/go-sql-driver/mysql" // Using MySQL driver
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
@@ -16,18 +15,16 @@ import (
 
 var db *sqlx.DB
 
-const tmplPath = "src/template/"
-
 var e = createMux()
 
 func main() {
 	db = connectDB()
+	repository.SetDB(db)
 	// `/` というパス（URL）と `articleIndex` という処理を結びつける
-	e.GET("/", articleIndex)
-
-	e.GET("/new", articleNew)
-	e.GET("/:id", articleShow)
-	e.GET("/:id/edit", articleEdit)
+	e.GET("/", handler.ArticleIndex)
+	e.GET("/new", handler.ArticleNew)
+	e.GET("/:id", handler.ArticleShow)
+	e.GET("/:id/edit", handler.ArticleEdit)
 
 	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
@@ -63,57 +60,57 @@ func connectDB() *sqlx.DB {
 	return db
 }
 
-func articleIndex(c echo.Context) error {
-	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
-	// return c.String(http.StatusOK, "Hello, World!")
-	data := map[string]interface{}{
-		"Message": "Article Index",
-		"Now":     time.Now(),
-	}
-	return render(c, "article/index.html", data)
-}
+// func articleIndex(c echo.Context) error {
+// 	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
+// 	// return c.String(http.StatusOK, "Hello, World!")
+// 	data := map[string]interface{}{
+// 		"Message": "Article Index",
+// 		"Now":     time.Now(),
+// 	}
+// 	return render(c, "article/index.html", data)
+// }
 
-func articleNew(c echo.Context) error {
-	data := map[string]interface{}{
-		"Message": "Article New",
-		"Now":     time.Now(),
-	}
+// func articleNew(c echo.Context) error {
+// 	data := map[string]interface{}{
+// 		"Message": "Article New",
+// 		"Now":     time.Now(),
+// 	}
 
-	return render(c, "article/new.html", data)
-}
+// 	return render(c, "article/new.html", data)
+// }
 
-func articleShow(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
+// func articleShow(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
 
-	data := map[string]interface{}{
-		"Message": "Article Show",
-		"Now":     time.Now(),
-		"ID":      id,
-	}
+// 	data := map[string]interface{}{
+// 		"Message": "Article Show",
+// 		"Now":     time.Now(),
+// 		"ID":      id,
+// 	}
 
-	return render(c, "article/show.html", data)
-}
+// 	return render(c, "article/show.html", data)
+// }
 
-func articleEdit(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
+// func articleEdit(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
 
-	data := map[string]interface{}{
-		"Message": "Article Edit",
-		"Now":     time.Now(),
-		"ID":      id,
-	}
+// 	data := map[string]interface{}{
+// 		"Message": "Article Edit",
+// 		"Now":     time.Now(),
+// 		"ID":      id,
+// 	}
 
-	return render(c, "article/edit.html", data)
-}
+// 	return render(c, "article/edit.html", data)
+// }
 
-func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
-	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
-}
+// func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+// 	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+// }
 
-func render(c echo.Context, file string, data map[string]interface{}) error {
-	b, err := htmlBlob(file, data)
-	if err != nil {
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	return c.HTMLBlob(http.StatusOK, b)
-}
+// func render(c echo.Context, file string, data map[string]interface{}) error {
+// 	b, err := htmlBlob(file, data)
+// 	if err != nil {
+// 		return c.NoContent(http.StatusInternalServerError)
+// 	}
+// 	return c.HTMLBlob(http.StatusOK, b)
+// }
