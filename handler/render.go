eda521371c4383ef3469237556b3490e35a25933
github Ryoma1115 <t9831703@gmail.com>
2020-08-15 13:59:07 +0900

    Modified handler/render.go and main.go.

--- handler/render.go
+++ handler/render.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/flosch/pongo2"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 const tmplPath = "src/template/"
--- main.go
+++ main.go
@@ -14,36 +14,30 @@ import (
 )
 
 var db *sqlx.DB
-
 var e = createMux()
 
 func main() {
 	db = connectDB()
 	repository.SetDB(db)
-	// `/` というパス（URL）と `articleIndex` という処理を結びつける
+
 	e.GET("/", handler.ArticleIndex)
 	e.GET("/new", handler.ArticleNew)
 	e.GET("/:id", handler.ArticleShow)
 	e.GET("/:id/edit", handler.ArticleEdit)
 
-	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func createMux() *echo.Echo {
-	// アプリケーションインスタンスを生成
 	e := echo.New()
 
-	// アプリケーションに各種ミドルウェアを設定
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
 
-	// `src/css` ディレクトリ配下のファイルに `/css` のパスでアクセスできるようにする
 	e.Static("/css", "src/css")
 	e.Static("/js", "src/js")
 
-	// アプリケーションインスタンスを返却
 	return e
 }
 
@@ -59,58 +53,3 @@ func connectDB() *sqlx.DB {
 	log.Println("db connection succeeded")
 	return db
 }
-
-// func articleIndex(c echo.Context) error {
-// 	// ステータスコード 200 で、"Hello, World!" という文字列をレスポンス
-// 	// return c.String(http.StatusOK, "Hello, World!")
-// 	data := map[string]interface{}{
-// 		"Message": "Article Index",
-// 		"Now":     time.Now(),
-// 	}
-// 	return render(c, "article/index.html", data)
-// }
-
-// func articleNew(c echo.Context) error {
-// 	data := map[string]interface{}{
-// 		"Message": "Article New",
-// 		"Now":     time.Now(),
-// 	}
-
-// 	return render(c, "article/new.html", data)
-// }
-
-// func articleShow(c echo.Context) error {
-// 	id, _ := strconv.Atoi(c.Param("id"))
-
-// 	data := map[string]interface{}{
-// 		"Message": "Article Show",
-// 		"Now":     time.Now(),
-// 		"ID":      id,
-// 	}
-
-// 	return render(c, "article/show.html", data)
-// }
-
-// func articleEdit(c echo.Context) error {
-// 	id, _ := strconv.Atoi(c.Param("id"))
-
-// 	data := map[string]interface{}{
-// 		"Message": "Article Edit",
-// 		"Now":     time.Now(),
-// 		"ID":      id,
-// 	}
-
-// 	return render(c, "article/edit.html", data)
-// }
-
-// func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
-// 	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
-// }
-
-// func render(c echo.Context, file string, data map[string]interface{}) error {
-// 	b, err := htmlBlob(file, data)
-// 	if err != nil {
-// 		return c.NoContent(http.StatusInternalServerError)
-// 	}
-// 	return c.HTMLBlob(http.StatusOK, b)
-// }
